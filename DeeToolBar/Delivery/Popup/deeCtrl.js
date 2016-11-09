32b096b37c49330b9e6c5d2ddd1b6928b8862927
bhoppenot <bhoppenot@oceanet-technology.com>
2016-11-09 07:59:06 +0100

    First commit

---
+++ DeeToolBar/Delivery/Popup/deeCtrl.js
@@ -0,0 +1,29 @@
+///<reference path="../typings/chrome.d.ts"/>
+chrome.storage.local.get('colour', function (res) {
+    document.getElementById("popupBody").setAttribute("style", "background-color:" + res["colour"] + ";");
+});
+document.addEventListener("click", function (e) {
+    var id = e.target['id'];
+    var alt = e.target['alt'];
+    if (id == 'captureKeys')
+        chrome.runtime.sendMessage({ action: 'toogleMediaKey' }, function (result) {
+            showMediaKeyStatus(result);
+        });
+    else
+        chrome.runtime.sendMessage({ action: alt });
+});
+chrome.runtime.sendMessage({ action: 'getMediaKey' }, function (result) {
+    showMediaKeyStatus(result);
+});
+function showMediaKeyStatus(status) {
+    var src = "../Content/";
+    src += status ? "on.png" : "off.png";
+    document.getElementById("captureKeys").setAttribute("src", src);
+}
+chrome.commands.getAll(function (commands) {
+    commands.forEach(function (command) {
+        if (command.description == "MediaPlayPause")
+            command.shortcut = "Ctrl+Shift+U";
+    });
+});
+//# sourceMappingURL=deeCtrl.js.map
---
+++ DeeToolBar/Popup/deeCtrl.ts
@@ -0,0 +1,33 @@
+///<reference path="../typings/chrome.d.ts"/>
+
+chrome.storage.local.get('colour', (res) => {
+    document.getElementById("popupBody").setAttribute("style", "background-color:" + res["colour"] + ";")
+});
+
+document.addEventListener("click", function (e: MouseEvent) {
+    var id = e.target['id'];
+    var alt = e.target['alt'];
+    if (id == 'captureKeys')
+        chrome.runtime.sendMessage({ action: 'toogleMediaKey' }, (result: boolean) => {
+            showMediaKeyStatus(result);
+        });
+    else
+        chrome.runtime.sendMessage({ action: alt });
+});
+
+chrome.runtime.sendMessage({ action: 'getMediaKey' }, (result: boolean) => {
+    showMediaKeyStatus(result);
+});
+
+function showMediaKeyStatus(status: boolean) {
+    var src = "../Content/";
+    src += status ? "on.png" : "off.png";
+    document.getElementById("captureKeys").setAttribute("src", src);
+}
+
+chrome.commands.getAll(function (commands) {
+    commands.forEach(function (command) {
+        if (command.description == "MediaPlayPause")
+            command.shortcut = "Ctrl+Shift+U";
+    });
+});
