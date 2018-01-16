a799d292ca73272029189c6a2d86a75d6ccd79e2
qerasmus <qerasmus@tsti.co.za>
2018-01-16 11:10:16 +0200

    Added functionality to search for lyrics.
    Signed-off-by: qerasmus <qerasmus@tsti.co.za>

--- DeeToolBar/content_scripts/actions.js
+++ DeeToolBar/content_scripts/actions.js
@@ -22,14 +22,23 @@ function execute(request, sender, callback) {
             document.getElementsByClassName('svg-icon-cancel')[0].parentElement.click();
             break;
         case 'Like':
-        case 'LikeStatus':
-            var like = document.getElementsByClassName('svg-icon-love-outline')[0];
-            var status = like.classList.value.indexOf('is-active') !== -1;
-            if (request.execute === "Like") {
-                like.parentElement.click();
-                status = !status;
-            }
-            callback(status);
+    	case 'LikeStatus':
+    		var like = document.getElementsByClassName('svg-icon-love-outline')[0];
+    		var status = like.classList.value.indexOf('is-active') !== -1;
+    		if (request.execute === "Like") {
+    			like.parentElement.click();
+    			status = !status;
+    		}
+    		callback(status);
+    		break;
+    	case 'Lyrics':
+    		var songName = document.getElementsByClassName('player-track-link')[0].innerHTML;
+    		var properSongName = songName.split(' ').join('-');
+    		var artist = document.getElementsByClassName('player-track-link')[1].innerHTML;
+    		var properArtistName = artist.split(' ').join('-');
+    		var newTab = window.open("http://www.1songlyrics.com/" + artist[0] + "/" + properArtistName + "/" + properSongName + ".html", '_blank');
+    		newTab.focus();
+    		break;
     }
 }
 
