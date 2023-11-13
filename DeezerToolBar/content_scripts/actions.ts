e0885b049054ed8552f696ef183fb05a1b844bb9
Benoit Hoppenot <benoit.hoppenot@agena3000.com>
2023-11-13 22:06:48 +0100

    Should work - need test

--- DeezerToolBar/content_scripts/actions.ts
+++ DeezerToolBar/content_scripts/actions.ts
@@ -2,33 +2,27 @@
 
 browser.runtime.onMessage.addListener(execute);
 
-
 function execute(request, sender, callback) {
     switch (request.execute) {
         case 'Play':
         case "PlayPause":
-            let pp = document.querySelector('.player-controls').childNodes[0];
-            pp = pp.childNodes[2].childNodes[0];
-            (<HTMLElement>pp).click();
+            let pp = document.querySelector("button[data-testid=play_button_play]")
+                ?? document.querySelector("button[data-testid=play_button_pause]");
+            (<HTMLElement>pp)?.click();
             break;
         case 'Playlist':
-            let openPlaylist = <HTMLElement>document.querySelector(
-                '.player-options .queuelist'
-            );
-            if (openPlaylist !== null)
-                openPlaylist.click();
+            let openPlaylist = <HTMLElement>document.querySelector("button[data-testid=queue_list_button]");
+            openPlaylist?.click();
             break;
         case 'Next':
         case "NextTrack":
-            let n = document.querySelector('.player-controls').childNodes[0];
-            n = n.childNodes[4].childNodes[0].childNodes[0];
-            (<HTMLElement>n).click();
+            let n = document.querySelector("button[data-testid=SkipNextFilledIcon]");
+            (<HTMLElement>n)?.click();
             break;
         case 'Prev':
         case "PrevTrack":
-            let p = document.querySelector('.player-controls').childNodes[0];
-            p = p.childNodes[0].childNodes[0].childNodes[0];
-            (<HTMLElement>p).click();
+            let p = document.querySelector("button[data-testid=SkipBackFilledIcon]")
+            (<HTMLElement>p)?.click();
             break;
         case 'Like':
         case 'Statuses':
@@ -41,17 +35,19 @@ function execute(request, sender, callback) {
 }
 
 function SetStatuses(request, callback) {
-    let like = <HTMLElement>document.querySelector('.track-actions').childNodes[0].childNodes[2].childNodes[0];
-    var status = like.children[0].classList.value.indexOf('eGhZom') !== -1;
+    let like = <HTMLElement>document.querySelector("button[data-testid=HeartOutlinedIcon]");
+    let unLike = <HTMLElement>document.querySelector("button[data-testid=HeartFilledIcon]");
+    let liked:bool = unLike !== null;
+    
     if (request.execute === "Like") {
-        like.click();
+        (notLiked ?? like).click();
         status = !status;
     }
 
-    let elt = document.querySelector('.marquee-content');
+    let elt = document.querySelector('.css-dsap3z');
 
     if (callback !== null)
-        callback({ status, playing: elt.textContent });
+        callback({ liked, playing: elt.textContent });
     else
         console.debug("Callback is null");
 }
